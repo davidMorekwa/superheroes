33e0c0144a5b8a94d698c83aa22e3d72fc1ddec4
Dave <70270638+davidMorekwa@users.noreply.github.com>
2023-04-15 23:56:44 +0300

    Navigation Animation Added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,24 +3,25 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.myapplication.model.Hero
-import com.example.myapplication.model.HeroesRepository
 import com.example.myapplication.model.Screens
 import com.example.myapplication.ui.logic.HeroViewModel
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import com.example.myapplication.ui.theme.Shapes
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,25 +40,35 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun HeroScreen() {
     var heroViewModel = HeroViewModel()
     var heroUiState = heroViewModel.heroUiState.collectAsState()
-    var navController = rememberNavController()
+//    var navController = rememberNavController()
+    var navController = rememberAnimatedNavController()
 
 
-
-    NavHost(
+    AnimatedNavHost(
         navController = navController,
         startDestination = Screens.ListPage.toString()
     ){
-        composable(route = Screens.ListPage.toString()){
+        composable(
+            route = Screens.ListPage.toString(),
+            enterTransition = { fadeIn() + slideInHorizontally() }
+        ){
             HeroesList(
                 viewModel = heroViewModel,
                 nav = navController
             )
         }
-        composable(route = Screens.HeroPage.toString()){
+        composable(
+            route = Screens.HeroPage.toString(),
+            enterTransition = {
+               fadeIn() + slideInHorizontally(spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow))
+            },
+            popExitTransition = { slideOutVertically(tween(300)) }
+        ){
             HeroPageScreen(
                 onBackClicked = {
                     navController.popBackStack(route = Screens.ListPage.toString(), inclusive = false)
